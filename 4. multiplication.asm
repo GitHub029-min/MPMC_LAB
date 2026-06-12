8aa071887370850d562dee5b1eeb039ac4ae9ab1
Mitu Barua Omi <mitubarua029@gmail.com>
2026-06-12 19:51:15 +0600

    Add files via upload
    MPMC Lab Codes

---
+++ 4. multiplication.asm
@@ -0,0 +1,53 @@
+.MODEL SMALL 
+.STACK 100H
+ 
+.DATA
+ 
+.CODE 
+MAIN PROC 
+	MOV AH, 1 
+	INT 21H  
+	SUB AL, 48 
+	MOV BL, AL 
+
+	MOV DL, 0DH 
+	MOV AH, 2  
+	INT 21H  
+	MOV DL, 0AH 
+	INT 21H 
+
+	MOV AH, 1 
+	INT 21H  
+	SUB AL, 48 
+	MOV CL, AL 
+
+	MOV DL, 0DH 
+	MOV AH, 2  
+	INT 21H  
+	MOV DL, 0AH 
+	INT 21H 
+
+	MOV AL, BL 
+	MUL CL 
+
+	MOV BX, AX 
+	MOV DL, 0 
+	MOV CX, 10 
+
+	DIV CX 
+
+	ADD AL, 48 
+	MOV DL, AL 
+	MOV AH, 2 
+	INT 21H 
+
+	MOV AL, AH 
+	ADD AL, 48 
+	MOV DL, AL 
+	MOV AH, 2 
+	INT 21H 
+	
+	MOV AH, 4CH 
+	INT 21H 
+MAIN ENDP 
+END MAIN
---
+++ README.md
@@ -0,0 +1,15 @@
+# Microprocessor & Microcontroller Lab Codes
+
+This repository contains assembly language programs for MPMC Lab (CSE 360).
+
+## Programs
+1. Input and Output
+2. Addition of Two Numbers
+3. Subtraction of Two Numbers
+4. Multiplication of Two Numbers
+5. Division of Two Numbers
+6. Sum of N Numbers using Loop
+7. Smallest of Two Numbers
+8. Smallest of Three Numbers
+9. Largest of Three Numbers
+10. Simple Pattern Printing
